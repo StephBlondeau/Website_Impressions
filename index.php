0ce7f1101e8ede1aabf4dd0d42c1906877ed0c9a
Stephanie Blondeau <stephanie.blondeau@laposte.net>
2014-06-11 09:23:25 +0200

    initial import

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<?php
+include ("./include/debut.inc.php");
+include("./include/fonctions.inc.php");
+
+?>
+<!--Contenue du corps du tableau-->
+    <tr>
+      <td rowspan="5">
+        <img src="images/index_17.jpg" width="33" height="463" alt="" />
+      </td>
+      <td colspan="11">
+          <div id="left_content"><br />
+            <div class="h2">Informations</div>
+            <br />
+	    <div class="content">
+              Debut de cr&eacute;ation : <br />
+              02 Juin 2014 <br /><br />
+            </div>
+            <div class="h2">Objectif</div>
+            <div class="content">
+              Sensibiliser les agents <br />
+              sur leur consommation <br />
+              de feuilles.<br />
+            </div>    
+        <!--<div class="h2">Find us <span class="style4">on</span></div>-->
+            </div>
+      </td>
+      <td colspan="8" rowspan="3">
+          <div id="content"><br />
+            <div class="h1">Bienvenue</div>
+            <br />
+            <div class="content"> Pr&eacutesentation :<br /><br />
+            A FAIRE...<br />
+            </div>
+          </div>
+      </td>
+      <td rowspan="5">
+	<img src="images/index_20.jpg" width="108" height="463" alt="" />
+      </td>
+    </tr>
+    <tr>
+      <td rowspan="2">
+        <img src="images/index_21.jpg" width="14" height="114" alt="" /></td>
+	    <td height="36" colspan="2" style="background:url(images/index_22.jpg)"></td>
+	    <td rowspan="2">
+	        <img src="images/index_23.jpg" width="11" height="114" alt="" /></td>
+	    <td style="background:url(images/index_24.jpg)"><img src="images/flikr-icon.png" width="36" height="36" border="0" alt=""/></td>
+	    <td colspan="2" rowspan="2">
+	        <img src="images/index_25.jpg" width="13" height="114" alt="" /></td>
+	    <td colspan="3" style="background:url(images/index_22.jpg)">
+	    </td>
+	    <td rowspan="2">
+	        <img src="images/index_27.jpg" width="35" height="114" alt="" /></td>
+      </tr>
+
+<?php
+include ("./include/fin.inc.php");
+?>
