d54a3648a6755c3735eda89bc546981b43dd96a8
Stephanie Blondeau <stephanie.blondeau@laposte.net>
2014-06-11 14:36:52 +0200

    Modification Rechercher

--- rechercher.php
+++ rechercher.php
@@ -2,18 +2,64 @@
 
 <?php
 include ("./include/debut.inc.php");
+//Ajout de la connexion à la BDD
+include ("./include/dbconnect.inc.php");
+//Ajout de la liste des fonctions
+include ("./include/fonctions.inc.php");
+
+//variable de base
+$code="";
+
+    if(isset ($_GET['code'])) //s'il y a un code
+    {
+        //*********on regarde si le code existe dans la base
+        $code="?code=".$_GET['code']; //variable à inséré dans l'url
+        $Code=$_GET['code'];
+    }
 ?>
 <!--Contenue du corps du tableau-->
     <tr>
+    <!--Debut du formulaire-->
+        <?php
+        echo "<form action='rechercher.php".$code."' method='POST'>";
+        ?>
       <td rowspan="5">
         <img src="images/index_17.jpg" width="33" height="463" alt="" />
       </td>
       <td colspan="11">
           <div id="left_content_image"><br />
+              <div class="h2"><br /></div>
+              <br />
+              <div class="content_statR">
+              <label for='titreD'> Titre du document : </label><br /><br />
+              </div>   
           </div>
       </td>
       <td colspan="8" rowspan="3">
           <div id="content"><br />
+              <div class="h1">Recherche d'un document</div>
+                  <br />
+                  <div class="content_statL"> 
+                      <input type='search' id='titreD' name='titre' placeholder='Veuillez entrer un titre'/>
+                      <input type="submit" id="valider" value="Rechercher"/>
+<?php
+                    if (isset ($_POST['titre']) and $code!="") //On vérifie si le formulaire a été valider (envoie du titre et du code
+                    {
+                        //On récupére la valeur du formulaire
+                        $recherche=$_POST['titre'];
+                        
+                        //on utilise une fonction pour rechercher les documentes qui compose cette recherche avec leur dates et le nombre de pages
+                        $resultat=rechercheDoc($recherche);
+                        
+                        //Création de 3 tableaux qui vont contenir les nom, date et pages des documents trouvés
+                        $dates=array();
+                        $noms=array();
+                        $pages=array();
+                        
+                    }
+
+?>
+                  </div>
           </div>
       </td>
       <td rowspan="5">
